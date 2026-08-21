336cb6d25325bd00dbe1976d95afc420898a0311
Richard von Rauchhaupt <richard.vonrauchhaupt@stonybrook.edu>
2026-08-21 14:12:58 -0400

    reconfigure upload dir

---
+++ symlinkFix.sh
@@ -0,0 +1,16 @@
+TARGET_FOLDER="uploads/moneal/Eudicot_Stem"
+
+find "$TARGET_FOLDER" -type l -lname '/home/tltsecure/apache2/htdocs/SIMPLE/*' | while read -r link; do
+  old_target=$(readlink "$link")
+  # Strip the legacy path prefix
+  rel_path="${old_target#/home/tltsecure/apache2/htdocs/SIMPLE/}"
+  # Compute relative path from the link's directory back to www/
+  link_dir=$(dirname "$link")
+  rel_to_root=$(realpath --relative-to="$link_dir" ".")
+  new_target="$rel_to_root/www/$rel_path"
+
+  echo "Rewriting: $link"
+  echo "  Old: $old_target"
+  echo "  New: $new_target"
+  ln -snf "$new_target" "$link"
+done
--- www/pages/online_examples.php
+++ www/pages/online_examples.php
@@ -18,41 +18,41 @@ Click on the links below to view finished image maps:
 <a class="sharedProject" href="includes/sea_life/SeaLifeLast.psd">source psd</a>
 --->
 <div id="linksPreview">
-  <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Amino_Acid/">Amino Acid</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Amphibian_Heart/">Amphibian Heart</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Bird_or_Mammal_Heart/">Bird or Mammal Heart</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/DNA_Structure/">DNA Structure</a>
+  <a class="sharedProject" href="/uploads/moneal/Amino_Acid/">Amino Acid</a>
+   <a class="sharedProject" href="/uploads/moneal/Amphibian_Heart/">Amphibian Heart</a>
+   <a class="sharedProject" href="/uploads/moneal/Bird_or_Mammal_Heart/">Bird or Mammal Heart</a>
+   <a class="sharedProject" href="/uploads/moneal/DNA_Structure/">DNA Structure</a>
 </div>
 
 <div id="linksPreview">
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Eudicot_Root/">Eudicot Root</a>
+   <a class="sharedProject" href="/uploads/moneal/Eudicot_Root/">Eudicot Root</a>
 <ul>
 <li>
-<a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Fish_Heart/?game=index">Fish Heart (Flash Card)</a>
+<a class="sharedProject" href="/uploads/moneal/Fish_Heart/?game=index">Fish Heart (Flash Card)</a>
 </li>
 
 <li>
-<a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Fish_Heart/?game=typing">Fish Heart (Typing)</a>
+<a class="sharedProject" href="/uploads/moneal/Fish_Heart/?game=typing">Fish Heart (Typing)</a>
 </li>
 
 <li>
-<a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Fish_Heart/">Fish Heart (Selector)</a>
+<a class="sharedProject" href="/uploads/moneal/Fish_Heart/">Fish Heart (Selector)</a>
 </li>
 
 </ul>
 
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Flower/">Flower</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Free_Energy_Diagram/">Free Energy Diagram</a>
+   <a class="sharedProject" href="/uploads/moneal/Flower/">Flower</a>
+   <a class="sharedProject" href="/uploads/moneal/Free_Energy_Diagram/">Free Energy Diagram</a>
  </div>
 
  <div id="linksPreview">
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Human_Excretory_System/">Human Excretory System</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Human_Respiratory_System/">Human Respiratory System</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Monocot_Root/">Monocot Root</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Monocot_Stem/">Monocot Stem</a>
+   <a class="sharedProject" href="/uploads/moneal/Human_Excretory_System/">Human Excretory System</a>
+   <a class="sharedProject" href="/uploads/moneal/Human_Respiratory_System/">Human Respiratory System</a>
+   <a class="sharedProject" href="/uploads/moneal/Monocot_Root/">Monocot Root</a>
+   <a class="sharedProject" href="/uploads/moneal/Monocot_Stem/">Monocot Stem</a>
  </div>
 
  <div id="linksPreview">
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Phylogenetic_Tree/">Phylogenetic Tree</a>
-   <a class="sharedProject" href="https://apps.tlt.stonybrook.edu/SIMPLE/uploads/moneal/Eudicot_Stem/">Eudicot Stem</a>
+   <a class="sharedProject" href="/uploads/moneal/Phylogenetic_Tree/">Phylogenetic Tree</a>
+   <a class="sharedProject" href="/uploads/moneal/Eudicot_Stem/">Eudicot Stem</a>
 </div>
